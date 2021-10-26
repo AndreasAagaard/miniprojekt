474df1f33cdfadf8d5d6c0998a16d0d4de3801ce
andreasskovaagaard <eaaasaa@students.eaaa.dk>
2021-10-26 10:46:53 +0200

    blazor til 5.0

---
+++ blazor/miniprojekt/miniprojekt/Client/Shared/Shelter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace miniprojekt.Client.Shared
+{
+    public class Shelter
+    {
+        private string xx;
+        private string xy;
+        private string yy;
+        private string yx;
+        private string aa;
+        private string bb;
+        private int kapacitet;
+        public Shelter()
+        {
+        }
+    }
+}
